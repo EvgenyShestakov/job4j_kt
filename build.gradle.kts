396dadc4195d876ad09cf2208bc50e3bf7550eb5
Evgeniy Shestakov <Evgeniy.Shestakov@rosbank.ru>
2025-05-29 12:18:47 +0300

    Configure Detekt and CI pipeline

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
-    kotlin("jvm") version "2.0.0"
+    kotlin("jvm") version "2.0.21"
+    id("io.gitlab.arturbosch.detekt") version "1.23.8"
 }
 
 group = "ru.job4j"
@@ -15,4 +16,19 @@ dependencies {
 
 tasks.test {
     useJUnitPlatform()
+}
+
+detekt {
+    toolVersion = "1.23.8"
+    config.from("$projectDir/config/detekt/detekt.yml")
+    buildUponDefaultConfig = true
+    allRules = false
+}
+
+tasks.withType<io.gitlab.arturbosch.detekt.Detekt> {
+    reports {
+        html.required.set(true)
+        xml.required.set(true)
+        txt.required.set(false)
+    }
 }
