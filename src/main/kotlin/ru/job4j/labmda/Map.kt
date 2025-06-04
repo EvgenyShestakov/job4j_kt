a7de1a00bf1ae2a213b942bbc1e159d3f5a9a6ae
Evgeniy Shestakov <Evgeniy.Shestakov@rosbank.ru>
2025-06-04 19:12:51 +0300

    Map

---
+++ src/main/kotlin/ru/job4j/labmda/Map.kt
@@ -0,0 +1,47 @@
+package ru.job4j.labmda
+
+import java.time.LocalDate
+
+fun main() {
+    val companies = listOf(
+        Company(
+            name = "Apple",
+            address = Address(
+                country = "USA",
+                city = "Cupertino",
+                street = "Apple Park Way",
+                building = "1"
+            ),
+            created = LocalDate.of(1976, 4, 1)
+        ),
+        Company(
+            name = "Google",
+            address = Address(
+                country = "USA",
+                city = "Mountain View",
+                street = "Amphitheatre Parkway",
+                building = "1600"
+            ),
+            created = LocalDate.of(1998, 9, 4)
+        ),
+        Company(
+            name = "Amazon",
+            address = Address(
+                country = "USA",
+                city = "Seattle",
+                street = "Leonard St",
+                building = "410 Terry Ave N"
+            ),
+            created = LocalDate.of(1994, 7, 5)
+        )
+    )
+    val companyViews = companies.map { """Company name ${it.name}.
+        | Address: ${it.address.country}, ${it.address.city}, ${it.address.street},
+        |  ${it.address.building}.
+        |   Create by ${it.created}""".trimMargin() }
+    companyViews.forEach{println(it)}
+}
+
+data class Company(val name: String, val address: Address, val created: LocalDate)
+
+data class Address(val country: String, val city: String, val street: String, val building: String)
---
+++ src/test/kotlin/ru/job4j/lambda/MapTest.kt
@@ -0,0 +1,67 @@
+package ru.job4j.lambda
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import ru.job4j.labmda.Address
+import ru.job4j.labmda.Company
+import java.time.LocalDate
+
+class MapTest {
+
+    @Test
+    fun whenCompaniesMapping() {
+        val expectedResult = listOf(
+            """Company name Apple.
+       | Address: USA, Cupertino, Apple Park Way,
+       |  1.
+       |   Create by 1976-04-01""".trimMargin(),
+            """Company name Google.
+       | Address: USA, Mountain View, Amphitheatre Parkway,
+       |  1600.
+       |   Create by 1998-09-04""".trimMargin(),
+            """Company name Amazon.
+       | Address: USA, Seattle, Leonard St,
+       |  410 Terry Ave N.
+       |   Create by 1994-07-05""".trimMargin()
+        )
+        val companies = listOf(
+            Company(
+                name = "Apple",
+                address = Address(
+                    country = "USA",
+                    city = "Cupertino",
+                    street = "Apple Park Way",
+                    building = "1"
+                ),
+                created = LocalDate.of(1976, 4, 1)
+            ),
+            Company(
+                name = "Google",
+                address = Address(
+                    country = "USA",
+                    city = "Mountain View",
+                    street = "Amphitheatre Parkway",
+                    building = "1600"
+                ),
+                created = LocalDate.of(1998, 9, 4)
+            ),
+            Company(
+                name = "Amazon",
+                address = Address(
+                    country = "USA",
+                    city = "Seattle",
+                    street = "Leonard St",
+                    building = "410 Terry Ave N"
+                ),
+                created = LocalDate.of(1994, 7, 5)
+            )
+        )
+        val companyViews = companies.map {
+            """Company name ${it.name}.
+        | Address: ${it.address.country}, ${it.address.city}, ${it.address.street},
+        |  ${it.address.building}.
+        |   Create by ${it.created}""".trimMargin()
+        }
+        assertThat(companyViews).isEqualTo(expectedResult)
+    }
+}
