2a7d6225ddcb610d6465c12ee3e6c91613c5ba68
Evgeniy Shestakov <Evgeniy.Shestakov@rosbank.ru>
2025-06-10 17:03:27 +0300

    Fixed HigherOrderFunctions

---
+++ src/main/kotlin/ru/job4j/condition/Action.kt
@@ -0,0 +1,36 @@
+package ru.job4j.condition
+
+fun createAction(): (Tracker, Input, Output) -> Boolean = { tracker, input, output ->
+    output.println("=== Create a new Item ====")
+    val name = input.ascString("Enter name: ")
+    val item = Item(name = name)
+    tracker.add(item)
+    true
+}
+
+fun deleteAction(): (Tracker, Input, Output) -> Boolean = { tracker, input, output ->
+    output.println("=== Delete item ===")
+    val id = input.ascInt("Enter Id: ")
+    when {
+        id == null -> output.println("Id must be a number")
+        tracker.delete(id) -> output.println("Request successfully deleted")
+        else -> output.println("Mistake. Request not found")
+    }
+    true
+}
+
+fun showAllAction(): (Tracker, Input, Output) -> Boolean = { tracker, input, output ->
+    output.println("=== Show all items ===")
+    val items = tracker.findAll()
+    if (items.isNotEmpty()) {
+        items.forEach { elem -> output.println(elem.toString()) }
+    } else {
+        output.println("No requests found")
+    }
+    true
+}
+
+fun exitAction(): (Tracker, Input, Output) -> Boolean = { _, _, output ->
+    output.println("=== Exit ===")
+    false
+}
--- src/main/kotlin/ru/job4j/condition/Main.kt
+++ src/main/kotlin/ru/job4j/condition/Main.kt
@@ -5,46 +5,11 @@ fun main() {
     val output = ConsoleOutput()
     val input = ConsoleInput(output)
 
-    val create: (Tracker, Input, Output) -> Boolean = { tracker, input, output ->
-        output.println("=== Create a new Item ====")
-        val name = input.ascString("Enter name: ")
-        val item = Item(name = name)
-        tracker.add(item)
-        true
-    }
-
-    val delete: (Tracker, Input, Output) -> Boolean = { tracker, input, output ->
-        output.println("=== Delete item ===")
-        val id = input.ascInt("Enter Id: ")
-        when {
-            id == null -> output.println("Id must be a number")
-            tracker.delete(id) -> output.println("Request successfully deleted")
-            else -> output.println("Mistake. Request not found")
-        }
-        true
-    }
-
-    val showAll: (Tracker, Input, Output) -> Boolean = { tracker, input, output ->
-        output.println("=== Show all items ===")
-        val items = tracker.findAll()
-        if (items.isNotEmpty()) {
-            items.forEach { elem -> println(elem) }
-        } else {
-            output.println("No requests found")
-        }
-        true
-    }
-
-    val exit: (Tracker, Input, Output) -> Boolean = { tracker, input, output ->
-        output.println("=== Exit ===")
-        false
-    }
-
     val userActions: Map<Int, Pair<String, (Tracker, Input, Output) -> Boolean>> = linkedMapOf(
-        0 to ("Create" to create),
-        1 to ("Delete" to delete),
-        2 to ("ShowAll" to showAll),
-        3 to ("Exit" to exit)
+        0 to ("Create" to createAction()),
+        1 to ("Delete" to deleteAction()),
+        2 to ("ShowAll" to showAllAction()),
+        3 to ("Exit" to exitAction())
     )
 
     StartUI(output).init(tracker, userActions, input)
