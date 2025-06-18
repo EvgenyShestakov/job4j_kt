1b0dd6ff8f5fe7ac540b5fa3f6c7f40cc565a41c
Evgeniy Shestakov <Evgeniy.Shestakov@rosbank.ru>
2025-06-18 18:32:58 +0300

    Fixed PromotionTest

--- src/test/kotlin/ru/job4j/safe/PromotionTest.kt
+++ src/test/kotlin/ru/job4j/safe/PromotionTest.kt
@@ -8,15 +8,31 @@ class PromotionTest {
 
     @Test
     fun whenPromotionNotEquals() {
-        val blackFridayPromo = Promotion("Black Friday", "US", LocalDate.of(2025, 1, 24))
-        val sale50Promo = Promotion("50% Off Everything", "US", LocalDate.of(2025, 4, 10))
+        val blackFridayPromo = Promotion(
+            "Black Friday",
+            "US",
+            LocalDate.of(2025, 1, 24)
+        )
+        val sale50Promo = Promotion(
+            "50% Off Everything",
+            "US",
+            LocalDate.of(2025, 4, 10)
+        )
         assertThat(blackFridayPromo).isNotEqualTo(sale50Promo)
     }
 
     @Test
     fun whenPromotionEquals() {
-        val blackFridayPromo = Promotion("Black Friday", "US", LocalDate.of(2025, 1, 24))
-        val blackFridayPromo2 = Promotion("Black Friday", "US", LocalDate.of(2025, 1, 24))
+        val blackFridayPromo = Promotion(
+            "Black Friday",
+            "US",
+            LocalDate.of(2025, 1, 24)
+        )
+        val blackFridayPromo2 = Promotion(
+            "Black Friday",
+            "US",
+            LocalDate.of(2025, 1, 24)
+        )
         assertThat(blackFridayPromo).isEqualTo(blackFridayPromo2)
     }
 }
