97ac4a303741c4960dd19afca98c50a3cc097c50
Evgeniy Shestakov <Evgeniy.Shestakov@rosbank.ru>
2025-06-30 15:57:09 +0300

    Concurrent search: Fixed

--- src/main/kotlin/ru/job4j/coroutines/CoroutineContextDemo.kt
+++ src/main/kotlin/ru/job4j/coroutines/CoroutineContextDemo.kt
@@ -1,87 +1,55 @@
 package ru.job4j.coroutines
 
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Deferred
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.async
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
-import kotlinx.coroutines.selects.select
-import kotlin.coroutines.cancellation.CancellationException
+import kotlinx.coroutines.withContext
+import kotlinx.coroutines.withTimeoutOrNull
+import java.util.concurrent.CountDownLatch
+import java.util.concurrent.atomic.AtomicReference
 
 fun main() {
-    val query = "replicaRequest"
-    fun searchByName(query: String): String = "search by name: $query"
-    fun searchByPhone(query: String): String = "search by phone: $query"
-    fun searchByEmail(query: String): String = "search by email: $query"
-    val searches: List<(String) -> String> = listOf(::searchByName, ::searchByPhone, ::searchByEmail)
     runBlocking {
-        println("Run with while: ${searchWithWhile(query, searches)}")
-        println("Run with select: ${searchWithSelect(query, searches)}")
+        val searches: List<suspend (String) -> String> = listOf(::slow, ::fast)
+        val result = search("request", searches)
+        println("Result: $result")
     }
 }
 
-suspend fun searchWithWhile(
+suspend fun search(
     query: String,
-    searchers: List<(String) -> String>,
+    searchers: List<suspend (String) -> String>,
 ): String = coroutineScope {
-
-    val deferreds: List<Deferred<String>> = launchSearchers(
-        query,
-        searchers
-    )
-
-    while (true) {
-        deferreds.forEach { job ->
-            if (job.isCompleted) {
-                try {
-                    val result = job.await()
-                    if (result.isNotEmpty()) {
-                        deferreds.forEach { if (!it.isCompleted) it.cancel() }
-                        return@coroutineScope result
-                    }
-                } catch (ex: CancellationException) {
-                    println("Coroutine wa cancelled ${ex.message}")
-                }
-            }
-        }
-        delay(10)
-    }
-
-    return@coroutineScope ""
-}
-
-suspend fun searchWithSelect(
-    query: String,
-    searchers: List<(String) -> String>,
-): String = coroutineScope {
-
-    val deferreds: List<Deferred<String>> = launchSearchers(
-        query,
-        searchers
-    )
-
-    return@coroutineScope select<String> {
-        deferreds.forEach { job ->
-            job.onAwait { answer ->
-                if (answer.isNotEmpty()) {
-                    deferreds.forEach { if (!it.isCompleted) it.cancel() }
-                    answer
-                } else {
-                    ""
-                }
+    val atomic = AtomicReference<String>()
+    val latch = CountDownLatch(1)
+    val jobs = searchers.map { searchFunction ->
+        launch(Dispatchers.IO) {
+            val rsl = searchFunction(query)
+            if (rsl.isNotEmpty() && latch.count > 0) {
+                atomic.set(rsl)
+                latch.countDown()
             }
         }
     }
+    withTimeoutOrNull(2000) {
+        withContext(Dispatchers.IO) {
+            latch.await()
+        }
+    }
+
+    jobs.forEach { it.cancel() }
+    atomic.get() ?: ""
 }
 
-private fun <T> CoroutineScope.launchSearchers(
-    query: String,
-    searchers: List<(String) -> T>,
-): List<Deferred<T>> = searchers.map { searchFunc ->
-    async(Dispatchers.IO) {
-        delay((200..500).random().toLong())
-        searchFunc(query)
-    }
+suspend fun fast(query: String): String {
+    delay(100)
+    return "search by fast: $query"
 }
+
+suspend fun slow(query: String): String {
+    delay(1000)
+    return "search by slow: $query"
+}
+
